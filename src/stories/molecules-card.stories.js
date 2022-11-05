aa1236b2b750e7f04207258023ed2369cfe1cc72
masato626 <m.takiguchi.22.7.561@4nonome.com>
2022-11-05 21:59:45 +0900

    cardを削除

--- src/stories/molecules-card.stories.js
+++
@@ -1,6 +0,0 @@
-import Item_Card from "../components/molecules/card/card.html";
-import "../components/molecules/card/card.scss";
-
-export default { title: "Molecules/Card" }
-
-export const Card = () => Item_Card;
