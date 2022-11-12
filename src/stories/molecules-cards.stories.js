f58842e8b8322a94c352c561fca3ad538dec3343
masato626 <m.takiguchi.22.7.561@4nonome.com>
2022-11-12 21:57:55 +0900

    card作成中

---
+++ src/stories/molecules-cards.stories.js
@@ -0,0 +1,5 @@
+import Item_Cards from "../components/molecules/card/card.html";
+import "../components/molecules/card/card.scss";
+
+export default { title: "Molecules/Cards" }
+export const Cards =() => Item_Cards
