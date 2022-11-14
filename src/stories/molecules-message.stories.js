5cbfb2e3968375251bf714c3cd60baff823bb752
masato626 <m.takiguchi.22.7.561@4nonome.com>
2022-11-14 16:59:41 +0900

    storiesを修正

--- src/stories/molecules-message.stories.js
+++ src/stories/molecules-message.stories.js
@@ -1,6 +1,6 @@
-import Item_Message from "../components/molecules/message/message.html";
+import Molecules_Message from "../components/molecules/message/message.html";
 import "../components/molecules/message/message.scss";
 
 export default { title: "Molecules/Message" }
 
-export const Message = () => Item_Message;
+export const Message = () => Molecules_Message;
