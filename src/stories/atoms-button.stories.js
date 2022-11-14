ad9f0bb23d1fc2f821fff8c0a56d8e4d9c87e373
masato626 <m.takiguchi.22.7.561@4nonome.com>
2022-11-14 16:54:37 +0900

    全体的に修正

--- src/stories/atoms-button.stories.js
+++
@@ -1,6 +0,0 @@
-import Item_Button from "../components/atoms/button/button.html";
-import "../components/atoms/button/button.scss";
-
-export default { title: "Atoms/Button" }
-
-export const Button = () => Item_Button;
