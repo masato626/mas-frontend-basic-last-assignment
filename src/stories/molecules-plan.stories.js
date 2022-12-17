aadc6dbc107b485c36026a756daaf3abaf2924d3
masato626 <m.takiguchi.22.7.561@4nonome.com>
2022-12-17 19:04:14 +0900

    プランを作成

---
+++ src/stories/molecules-plan.stories.js
@@ -0,0 +1,6 @@
+import Molecules_Plan from "../components/molecules/plan/plan.html";
+import "../components/molecules/plan/plan.scss";
+
+export default { title: "Molecules/Plan" }
+
+export const Button = () => Molecules_Plan;
