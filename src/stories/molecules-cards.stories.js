d103a54ab63e76f5d1b043b89dad1ad9cc08fcaa
masato626 <m.takiguchi.22.7.561@4nonome.com>
2023-01-04 15:55:40 +0900

    kannsei

--- src/stories/molecules-cards.stories.js
+++ src/stories/molecules-cards.stories.js
@@ -2,4 +2,4 @@ import Item_Card from "../components/molecules/card/card.html";
 import "../components/molecules/card/card.scss";
 
 export default { title: "Molecules/Card" }
-export const Cards =() => Item_Card
+export const Card =() => Item_Card
