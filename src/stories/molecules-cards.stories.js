c4c94735c1e1d4373256bcd22bd6772724f2c4a0
masato626 <m.takiguchi.22.7.561@4nonome.com>
2023-01-04 15:30:05 +0900

    全体的に修正

--- src/stories/molecules-cards.stories.js
+++ src/stories/molecules-cards.stories.js
@@ -1,5 +1,5 @@
-import Item_Cards from "../components/molecules/card/card.html";
+import Item_Card from "../components/molecules/card/card.html";
 import "../components/molecules/card/card.scss";
 
-export default { title: "Molecules/Cards" }
-export const Cards =() => Item_Cards
+export default { title: "Molecules/Card" }
+export const Cards =() => Item_Card
