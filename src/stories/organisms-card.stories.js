0a31300e8ab5b077a11f3cc3f3e79761baa26a08
masato626 <m.takiguchi.22.7.561@4nonome.com>
2023-01-19 17:10:47 +0900

    完成

---
+++ src/stories/organisms-card.stories.js
@@ -0,0 +1,6 @@
+import Item_Card from "../components/organisms/card/card.html";
+import "../components/organisms/card/card.scss";
+
+export default { title: "Organisms/Card" };
+
+export const Card = () => Item_Card;
