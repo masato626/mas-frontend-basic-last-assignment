13de1c0d68720d303d62460f6fc2792cd9c02436
masato626 <m.takiguchi.22.7.561@4nonome.com>
2023-01-31 15:30:18 +0900

    kannsei

---
+++ src/stories/organisms-footer.stories.js
@@ -0,0 +1,5 @@
+import Item_Footer from "../components/organisms/footer/footer.html";
+import "../components/organisms/footer/footer.scss";
+
+export default { title: "Organisms/Footer" };
+export const Footer = () => Item_Footer;
