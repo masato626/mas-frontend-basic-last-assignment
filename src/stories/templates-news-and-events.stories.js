3589dee37f1ddd4444ea9a3e35ee470d685284dd
masato626 <m.takiguchi.22.7.561@4nonome.com>
2023-02-01 16:17:06 +0900

    完成

---
+++ src/stories/templates-news-and-events.stories.js
@@ -0,0 +1,6 @@
+import Item_Newsandevents from "../components/templates/news-and-events/news-and-events.html";
+import "../components/templates/news-and-events/news-and-events.scss";
+
+export default { title: "Templates/News-and-events" };
+
+export const Events = () => Item_Newsandevents;
